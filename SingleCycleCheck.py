0d857b76198fc96a424a324001440d7b42718abe
Josh Madakor <josh.madakor@gmail.com>
2022-03-08 11:43:14 -0800

    Added SingleCycleCheck

---
+++ SingleCycleCheck.py
@@ -0,0 +1,29 @@
+'''
+    Single Cycle Check:
+
+    Consider an array of integers where each integer represents a jump of its value
+    in the array. If a jump spills past either bounds, it will wrap around to the 
+    other side of the array. Write a function that determines whether the jumps form
+    a single cycle or not. A cycle occurs if starting at any index in the array and
+    following the jumps, every element in the array is visited exactly once before 
+    landing back on the starting index
+
+    Time:  O(N), where N is the number of elements in the array
+    Space: O(C)
+'''
+def hasSingleCycle(array):
+    currentIndex = 0
+    for i in range(len(array)):
+        if currentIndex == 0 and i > 0:
+            return False
+        currentIndex = getNextIndex(currentIndex, array)
+    return currentIndex == 0
+
+# Helper function to get the next index  
+def getNextIndex(currentIndex, array):
+    nextIndex = (currentIndex + array[currentIndex]) % len(array)
+    if nextIndex < 0:
+        return len(array) + nextIndex
+    return nextIndex
+
+assert hasSingleCycle([2, 3, 1, -4, -4, 2]) == True, "Error. Expected output: True"
