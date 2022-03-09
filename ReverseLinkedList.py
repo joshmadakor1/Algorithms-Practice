bd6726dba40129d274da41b61840c15f1899ea68
Josh Madakor <josh.madakor@gmail.com>
2022-03-09 07:09:04 -0800

    Added ReverseLinkedList

---
+++ ReverseLinkedList.py
@@ -0,0 +1,24 @@
+'''
+    Reverse Linked List:
+    Given a linked list, reverse it and return the new head node
+
+    Time:  O(N), where N are the number of nodes in the list
+    Space: O(3) -> O(1), three pointers are used for reversal operations
+
+    Last Practice: 2022-03-09 07:07:19
+'''
+class LinkedList:
+	def __init__(self, value):
+		self.value = value
+		self.next = None
+		
+def reverseLinkedList(head):
+	currentNode = head
+	previousNode = None
+	
+	while currentNode is not None:
+		nextNode = currentNode.next
+		currentNode.next = previousNode
+		previousNode = currentNode
+		currentNode = nextNode
+	return previousNode
