d19eef8084ced6c4dfed77dcfdc684fb8e295b10
Josh Madakor <josh.madakor@gmail.com>
2022-03-13 09:08:09 -0700

    Practiced CaesarCipherEncryptor

---
+++ CaesarCipherEncryptor.py
@@ -0,0 +1,23 @@
+'''
+    Caesar Cipher Encryptor:
+    Given a string and a key to serve as an offset, encrypt the
+    string with a Caesar Cipher and return the new ciphertext.
+
+    Time:  O(N), where N = number of characters in the string
+    Space: O(1)
+    
+    Last Practice: 2022-03-13 09:05:49
+'''
+def caesarCipherEncryptor(string, key):
+	key %= 26
+	ciphertext = []
+	for char in string:
+		ciphertext.append(encipher(char, key))
+	return "".join(ciphertext)
+
+def encipher(char, key):
+	start = ord('a') - 1
+	end = ord('z')
+	newChar = ord(char) + key
+	if newChar > end: return chr((newChar % end) + start)
+	return chr(newChar)
--- scratch.py
+++ scratch.py
@@ -1,7 +1,2 @@
-from heapq import heapify
-
-
-test = [5,3,6,8,4,2,6]
-heapify(test)
-print(test[0])
-print(test.pop(0))
+print(-5%10)
+print(1%10)
