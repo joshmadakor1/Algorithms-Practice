d97e29da8c39c082d6ef77e9985092e76fa28348
Josh Madakor <josh.madakor@gmail.com>
2022-03-13 07:36:31 -0700

    Practiced ReconstructBST

---
+++ ReconstructBst.py
@@ -0,0 +1,27 @@
+'''
+    Reconstruct BST:
+    Reconstruct a binary search tree given an array that represents a
+    pre-order traversal of a BST
+
+    Time: O(N), where N is the number of elements in the array
+    Space: O(N), where N isi the numbef of elements in the array (unbalanced BST) 
+
+    Last Practice: 2022-03-12 17:02:09
+'''
+class BST:
+	def __init__(self, value, left=None, right=None):
+		self.value = value
+		self.left = left
+		self.right = right
+	
+def reconstructBst(array):
+	if not len(array): return None
+	currentValue = array[0]
+	rightIndex = len(array)
+	for i in range(1,len(array)):
+		if array[i] >= currentValue:
+			rightIndex = i
+			break
+	leftBranch = reconstructBst(array[1:rightIndex])
+	rightBranch = reconstructBst(array[rightIndex:])
+	return BST(currentValue, leftBranch, rightBranch)
