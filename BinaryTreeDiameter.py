ccd368915e5a0f8b25a07cd58587628c62bc581d
Josh Madakor <josh.madakor@gmail.com>
2022-03-15 07:55:24 -0700

    Practiced BinaryTreediameter

--- BinaryTreeDiameter.py
+++ BinaryTreeDiameter.py
@@ -7,15 +7,15 @@
     Time:  O(N)
     Space: O(N), (Alternate O(D), where D is the depth of binary tree)
 
-	Last Practiced: 2022-03-10 06:09:19
+	Last Practiced: 2022-03-15 07:55:12
 '''
+# This is an input class. Do not edit.
 class BinaryTree:
     def __init__(self, value, left=None, right=None):
         self.value = value
         self.left = left
         self.right = right
-
-# Helper class to keep track of the Diameter and Height of all the nodes
+		
 class TreeInfo:
 	def __init__(self, diameter, height):
 		self.diameter = diameter
@@ -25,15 +25,12 @@ def binaryTreeDiameter(tree):
     return getTreeInfo(tree).diameter
 
 def getTreeInfo(tree):
-	if tree is None:
-		return TreeInfo(0,0)
-	
+	if tree is None: return TreeInfo(0,0)
 	leftBranchInfo = getTreeInfo(tree.left)
 	rightBranchInfo = getTreeInfo(tree.right)
-	
-	pathLengthThroughRoot = leftBranchInfo.height + rightBranchInfo.height
-	totalDiameterSoFar = max(leftBranchInfo.diameter, rightBranchInfo.diameter)
-	currentDiameter = max(pathLengthThroughRoot,totalDiameterSoFar)
+	maxPathThroughRoot = leftBranchInfo.height + rightBranchInfo.height
+	maxWidthSoFar = max(leftBranchInfo.diameter, rightBranchInfo.diameter)
+	currentDiameter = max(maxPathThroughRoot, maxWidthSoFar)
 	currentHeight = 1 + max(leftBranchInfo.height, rightBranchInfo.height)
-	
 	return TreeInfo(currentDiameter, currentHeight)
+
