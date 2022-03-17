5c84e3e1833b05133c972a92c94cd1e268e068e1
Josh Madakor <josh.madakor@gmail.com>
2022-03-17 09:09:12 -0700

    Practiced NodeDepths and ProductSum

---
+++ NodeDepths.py
@@ -0,0 +1,25 @@
+'''
+    Node Depths:
+
+    Given an binary tree as an input, sum up the depths of all the nodes and return
+    the depth as an integer. Assume the root of the tree has a depth of 0.
+
+    Time:  O(N), where N is the number of nodes in the tree
+    Space: O(D) -> O(N), where D is the depth of the binary tree
+
+    LastPracticed: 2022-03-17 09:08:54
+'''
+def nodeDepths(root, depth = 0):
+    if root is None: return 0
+    left = nodeDepths(root.left, depth + 1)
+    right = nodeDepths(root.right, depth + 1)
+    depth += (left+right)
+    return depth
+
+
+# This is the class of the input binary tree.
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
--- ProductSum.py
+++ ProductSum.py
@@ -1,24 +1,21 @@
 '''
-	Product Sum:
-	Write a function that takes in a special array which contains
-	integers and other nested arrays of integers. The product sum
-	is the sum of the arrays elements, multiplied by the level of
-	the depth of the nested arrays.
+    Product Sum:
+    Write a function that takes in a special array which contains
+    integers and other nested arrays of integers. The product sum
+    is the sum of the arrays elements, multiplied by the level of
+    the depth of the nested arrays.
 
-	Time:  O(N) - Linear
-	Space: O(D) -> O(N), where D is the depth of the recursive call stack - Linear
+    Time:  O(N) - Linear
+    Space: O(D) -> O(N), where D is the depth of the recursive call stack - Linear
 '''
-def productSum(array):
-    rootDepth = 1
-    return calculateProductSum(array, rootDepth)
+def productSum(array, depth = 1):
+    sum = 0
+    for element in array:
+        if type(element) == type([]):
+            sum += productSum(element, depth + 1)
+        else:
+            sum += element
+    return sum * depth
 
-def calculateProductSum(array, depth):
-	sum = 0
-	for element in array:
-		if type(element) == type([]):
-			sum += calculateProductSum(element, depth + 1)
-		else:
-			sum += element
-	return sum * depth
 
 assert productSum([5, 1, [7, -1], 3, [6, [-13, 8], 4]]) == 11, "Error. Expected output: 11"
