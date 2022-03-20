66d68c0e971253e1a4250d917698bc938855e3c7
Josh Madakor <josh.madakor@gmail.com>
2022-03-20 07:40:21 -0700

    Practiced ShiftedBinarySearch

--- ShiftedBinarySearch.py
+++ ShiftedBinarySearch.py
@@ -6,23 +6,26 @@
     Time: O(N)
     Space: O(1)
 
-    Last Practiced: 2022-03-20 07:25:49
+    Last Practiced: 2022-03-20 07:40:00
 '''
 def shiftedBinarySearch(array, target):
-    return search(array, 0, len(array)-1, target)
+    return search(array, target, 0, len(array)-1)
 
-
-def search(array, left, right, target):
+def search(array, target, left, right):
 	if left > right: return -1
 	mid = (left+right)//2
 	if array[mid] == target: return mid
 	
+	# Left is Sorted
 	if array[left] <= array[mid]:
 		if target >= array[left] and target < array[mid]:
-			return search(array, left, mid-1, target)
-		else: return search(array, mid+1, right, target)
-	elif array[left] >= array[mid]:
+			return search(array, target, left, mid-1)
+		else:
+			return search(array, target, mid+1, right)
+	# Right is Sorted
+	else:
 		if target > array[mid] and target <= array[right]:
-			return search(array, mid+1, right, target)
-		else: return search(array, left, mid-1, target)
+			return search(array, target, mid+1, right)
+		else:
+			return search(array, target, left, mid-1)
 	
