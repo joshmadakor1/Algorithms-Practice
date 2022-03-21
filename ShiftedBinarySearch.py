112dfa498d626779b5ea5c78a160a6a2f24dcb4a
Josh Madakor <josh.madakor@gmail.com>
2022-03-21 05:42:46 -0700

    Practiced ShiftedBinarySearch

--- ShiftedBinarySearch.py
+++ ShiftedBinarySearch.py
@@ -6,26 +6,27 @@
     Time: O(N)
     Space: O(1)
 
-    Last Practiced: 2022-03-20 07:40:00
+    Last Practiced: 2022-03-21 05:42:20
 '''
 def shiftedBinarySearch(array, target):
-    return search(array, target, 0, len(array)-1)
+    return search(array, 0, len(array)-1, target)
 
-def search(array, target, left, right):
+
+def search(array, left, right, target):
 	if left > right: return -1
 	mid = (left+right)//2
 	if array[mid] == target: return mid
 	
-	# Left is Sorted
+	# Left is in order
 	if array[left] <= array[mid]:
 		if target >= array[left] and target < array[mid]:
-			return search(array, target, left, mid-1)
+			return search(array, left, mid-1, target)
 		else:
-			return search(array, target, mid+1, right)
-	# Right is Sorted
+			return search(array, mid+1, right, target)
+	# Right is in order
 	else:
 		if target > array[mid] and target <= array[right]:
-			return search(array, target, mid+1, right)
+			return search(array, mid+1, right, target)
 		else:
-			return search(array, target, left, mid-1)
+			return search(array, left, mid-1, target)
 	
--- scratch.py
+++ scratch.py
@@ -1,46 +1,17 @@
+resultValue = 0  # Create a variable called "resultValue" and set it to 0
+firstValue = 0   # Create a variable called "firstValue" and set it to 0
+secondValue = 0  # Create a variable called "secondValue" and set it to 0
 
-# Returns true if the
-# there is a subarray
-# of arr[] with sum
-# equal to 'sum' 
-# otherwise returns
-# false. Also, prints
-# the result 
+firstValue = input()  # Type a number at the command prompt and press enter, it will go in here
+secondValue = input() # Same as above line
 
+# 'int()' attempts to force whatever is in the () to become a number
+# 'secondValue' is subtracted from 'firstValue' (the things you entered on the command line)
+# the result is stored in 'resultValue'
+resultValue = int(firstValue) - int(secondValue) 
 
-def subArraySum(arr, n, sum_):
-    count = 0
-    # Pick a starting 
-    # point
-    for i in range(n):
-        curr_sum = arr[i]
-    
-        # try all subarrays
-        # starting with 'i'
-        j = i + 1
-        while j <= n:
-            count +=1
-            print("count,", count)
-            if curr_sum == sum_:
-                print ("Sum found between")
-                print("indexes % d and % d"%( i, j-1))
-                
-                return 1
-                
-            if curr_sum > sum_ or j == n:
-                break
-            
-            curr_sum = curr_sum + arr[j]
-            j += 1
+# "Result: " is printed to the console
+print("Result: ")
 
-    print ("No subarray found")
-    return 0
-
-# Driver program 
-arr = [2, 4, 8, 5, 9, 10, 15, 99]
-n = len(arr)
-sum_ = 23
-
-subArraySum(arr, n, sum_)
-
-# This code is Contributed by shreyanshi_arun.
+# whatever the value of 'resultValue' is, is printed to the console
+print(resultValue)
