b83d96a430e20533f798b5ca92402823f6dbc41f
Josh Madakor <josh.madakor@gmail.com>
2022-03-21 07:29:40 -0700

    Practiced ReconstnructBst_PreOrderArray

--- ReconstructBst_PreOrderArray.py
+++ ReconstructBst_PreOrderArray.py
@@ -1,10 +1,13 @@
 '''
-	Reconstruct BST (Preorder Array):
+    Reconstruct BST (Preorder Array):
     Given an input array ordered in preorder, reconstruct a binary tree
 
     Time:  O(N)
     Space: O(N), where N is the length of the input array
+
+    Last PracticedL 2022-03-21 07:29:24
 '''
+# This is an input class. Do not edit.
 class BST:
     def __init__(self, value, left=None, right=None):
         self.value = value
@@ -14,14 +17,14 @@ class BST:
 
 def reconstructBst(preOrderTraversalValues):
     if len(preOrderTraversalValues) == 0:
-        return
+        return None
     
     currentValue = preOrderTraversalValues[0]
     rightIndex = len(preOrderTraversalValues)
     
-    for index in range(1, len(preOrderTraversalValues)):
-        if preOrderTraversalValues[index] >= currentValue:
-            rightIndex = index
+    for i in range(1, len(preOrderTraversalValues)):
+        if preOrderTraversalValues[i] >= currentValue:
+            rightIndex = i
             break
     
     leftBranch = reconstructBst(preOrderTraversalValues[1:rightIndex])
