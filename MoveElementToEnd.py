a655194667f5176b7dcf56cf40a6bfb8d9a56a0b
Josh Madakor <josh.madakor@gmail.com>
2022-03-23 07:24:54 -0700

    Practiced MoveElementtoEnd

--- MoveElementToEnd.py
+++ MoveElementToEnd.py
@@ -1,24 +1,22 @@
 '''
-	Move Element To End:
+    Move Element To End:
     Given an input array and an input value, move all elements
     that are equal to the input value to the end of the array
 
     Time: O(N), where N is the number of elements in the array
     Space: O(1), mutate existing array
 
-    Last Practiced: 2022-03-11 11:19:44
+    Last Practiced: 2022-03-23 07:24:42
 '''
 def moveElementToEnd(array, toMove):
     left = 0
     right = len(array) - 1
     
     while left < right:
-        while left < right and array[right] == toMove:
+        while array[right] == toMove and left < right:
             right -= 1
         if array[left] == toMove:
             array[left], array[right] = array[right], array[left]
-            right -= 1
-        else:
-            left += 1
+        left += 1
     
     return array
