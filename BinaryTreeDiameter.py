648831caa09e0fa85440cbc9d94457d4402c8e73
Josh Madakor <josh.madakor@gmail.com>
2022-03-23 07:19:40 -0700

    Practiced BinaryTreeDiameter

--- BinaryTreeDiameter.py
+++ BinaryTreeDiameter.py
@@ -7,30 +7,34 @@
     Time:  O(N)
     Space: O(N), (Alternate O(D), where D is the depth of binary tree)
 
-	Last Practiced: 2022-03-15 07:55:12
+	Last Practiced: 2022-03-23 07:19:27
 '''
+# This is an input class. Do not edit.
 # This is an input class. Do not edit.
 class BinaryTree:
     def __init__(self, value, left=None, right=None):
         self.value = value
         self.left = left
         self.right = right
-		
+
 class TreeInfo:
 	def __init__(self, diameter, height):
 		self.diameter = diameter
 		self.height = height
-
+		
 def binaryTreeDiameter(tree):
     return getTreeInfo(tree).diameter
 
 def getTreeInfo(tree):
-	if tree is None: return TreeInfo(0,0)
-	leftBranchInfo = getTreeInfo(tree.left)
-	rightBranchInfo = getTreeInfo(tree.right)
-	maxPathThroughRoot = leftBranchInfo.height + rightBranchInfo.height
-	maxWidthSoFar = max(leftBranchInfo.diameter, rightBranchInfo.diameter)
-	currentDiameter = max(maxPathThroughRoot, maxWidthSoFar)
-	currentHeight = 1 + max(leftBranchInfo.height, rightBranchInfo.height)
+	if tree is None:
+		return TreeInfo(0,0)
+	leftTreeInfo = getTreeInfo(tree.left)
+	rightTreeInfo = getTreeInfo(tree.right)
+	
+	maxPathOverRoot = leftTreeInfo.height + rightTreeInfo.height
+	maxDiameterSoFar = max(leftTreeInfo.diameter, rightTreeInfo.diameter)
+	currentDiameter = max(maxPathOverRoot, maxDiameterSoFar)
+	currentHeight = 1 + max(leftTreeInfo.height, rightTreeInfo.height)
+	
 	return TreeInfo(currentDiameter, currentHeight)
 
