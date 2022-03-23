d6cb9d0d2ff450706b100d02479fb322f8a7d5b0
Josh Madakor <josh.madakor@gmail.com>
2022-03-23 06:39:34 -0700

    Practiced ShiftedBinarySearch

--- ShiftedBinarySearch.py
+++ ShiftedBinarySearch.py
@@ -6,30 +6,28 @@
     Time: O(N)
     Space: O(1)
 
-    Last Practiced: 2022-03-22 06:09:27
+    Last Practiced: 2022-03-23 06:39:13
 '''
 def shiftedBinarySearch(array, target):
-	return search(array, target, 0, len(array) -1)
+    return search(array, target, 0, len(array) - 1)
 
 def search(array, target, left, right):
 	if left > right: return -1
 	mid = (left+right)//2
 	if array[mid] == target: return mid
 
-	# If the left side is sorted
+	# Left side is sorted
 	if array[left] <= array[mid]:
-		# If the target is in the left side:
-		if target < array[mid] and target >= array[left]:
-			return search(array, target, 0, mid - 1)
-		# If the target is in the right side:
+		if target >= array[left] and target < array[mid]:
+			return search(array, target, left, mid - 1)
 		else:
 			return search(array, target, mid + 1, right)
-	# If the right side is sorted
-	else:
-		# If the target is in the right side:
+	# Right side is sorted
+	elif array[mid] <= array[right]:
 		if target > array[mid] and target <= array[right]:
 			return search(array, target, mid + 1, right)
-		# If the target is in the left side:
 		else:
 			return search(array, target, left, mid - 1)
+	
+
 	
--- scratch.py
+++ scratch.py
@@ -1,25 +1,112 @@
-def waterArea(heights):
-    largestLeft = [0] * len(heights)
-    largestRight = [0] * len(heights)
-    
-    largestSoFar = 0
-    for i in range(len(heights)):
-        largestLeft [i]= largestSoFar
-        largestSoFar = max(heights[i], largestSoFar)
-    
-    largestSoFar = 0
-    for i in reversed(range(len(heights))):
-        largestRight[i] = largestSoFar
-        largestSoFar = max(heights[i], largestSoFar)
+'''
+    Min Heap Construction:
+    Build a min heap given an input array and implement the
+    following functionality:
+
+    buildHeap
+    insert
+    remove
+    peek
+    siftUp
+    siftDown
+    swap
+
+    Last Practiced: 2022-03-16 07:50:50
+'''
+class MinHeap:
+    def __init__(self, array):
+        # Do not edit the line below.
+        self.heap = self.buildHeap(array)
+
+    def buildHeap(self, array):
+        firstParent = (len(array) - 2) // 2
+        for i in reversed(range(firstParent + 1)):
+            self.siftDown(array, i, len(array) - 1)
+        return array
+
+    def siftDown(self, heap, currentIndex, endIndex):
+        childOneIndex = currentIndex * 2 + 1
+        while childOneIndex <= endIndex:
+            childTwoIndex = currentIndex * 2 + 2 if currentIndex * 2 + 2 <= endIndex else -1
+            if childTwoIndex != -1 and heap[childTwoIndex] < heap[childOneIndex]:
+                indexToSwap = childTwoIndex
+            else:
+                indexToSwap = childOneIndex
+            if heap[indexToSwap] < heap[currentIndex]:
+                self.swap(heap, indexToSwap, currentIndex)
+                currentIndex = indexToSwap
+                childOneIndex = currentIndex * 2 + 1
+            else:
+                return # heap is already in built
+
+    def siftUp(self, heap, currentIndex):
+        parentIndex = (currentIndex - 1) // 2
+        while parentIndex >= 0 and heap[parentIndex] > heap[currentIndex]:
+            self.swap(heap, currentIndex, parentIndex)
+            currentIndex = parentIndex
+            parentIndex = (currentIndex - 1) // 2
+
+    def peek(self):
+        if len(self.heap) >= 1: return self.heap[0]
+
+    def remove(self):
+        self.swap(self.heap, 0, len(self.heap) - 1)
+        valueToPop = self.heap.pop()
+        self.siftDown(self.heap, 0, len(self.heap) - 1)
+        return valueToPop
+
+    def insert(self, value):
+        self.heap.append(value)
+        self.siftUp(self.heap, len(self.heap) - 1)
         
-    totalArea = 0
+    def swap(self, heap, left, right):
+        heap[left], heap[right] = heap[right], heap[left]
 
-    for i in range(len(heights)):
-        walls = min(largestLeft[i], largestRight[i])
-        if heights[i] < walls:
-            totalArea += walls - heights[i]
-
-    return totalArea
-
+def dijkstrasAlgorithm(start, edges):
+    numberOfEdges = len(edges)
+    minDistanceFromStart = [float('inf')] * numberOfEdges
+    minDistanceFromStart[start] = 0
+    visited = set()
     
-waterArea([0, 8, 0, 0, 5, 0, 0, 10, 0, 0, 1, 1, 0, 3])
+    while len(visited) < numberOfEdges:
+        currentVertex, distanceFromStartToCurrentVertex = getNextClosestUnvisitedNode(minDistanceFromStart, visited)
+        visited.add(currentVertex)
+        
+        if distanceFromStartToCurrentVertex == float('inf'): break # Closest node is unreachable. Finish algorithm
+        
+        for edge in edges[currentVertex]:
+            currentVertextNeighbor, distanceFromCurrentVertexToNeighbor = edge
+            if currentVertextNeighbor in visited: continue
+            
+            newDistanceFromStartToNeighbor = distanceFromStartToCurrentVertex + distanceFromCurrentVertexToNeighbor
+            currentDistanceFromStartToNeighbor = minDistanceFromStart[currentVertextNeighbor]
+            
+            if newDistanceFromStartToNeighbor < currentDistanceFromStartToNeighbor:
+                minDistanceFromStart[currentVertextNeighbor] = newDistanceFromStartToNeighbor
+                
+    return list(map(lambda x: -1 if x == float('inf') else x, minDistanceFromStart))
+
+def getNextClosestUnvisitedNode(minDistanceFromStart, visited):
+    candidateVertex = None
+    candidateDistance = float('inf')
+    
+    for vertex, distance in enumerate(minDistanceFromStart):
+        if vertex in visited: continue
+        if distance <= candidateDistance:
+            candidateVertex = vertex
+            candidateDistanxe = distance
+            
+    return candidateVertex, candidateDistance
+
+
+distances = [
+    [[1,7]],
+    [[2,6],[3,20],[4,3]],
+    [[3,14]],
+    [[4,2]],
+    [],
+    []
+    ]
+start = 0
+
+print(dijkstrasAlgorithm(start, distances))
