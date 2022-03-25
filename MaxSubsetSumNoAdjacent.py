c688c860945653242d7253995e2769d3db818c78
Josh Madakor <josh.madakor@gmail.com>
2022-03-25 05:50:24 -0700

    Practiced MaxSubsetSumNoAdjacent

--- MaxSubsetSumNoAdjacent.py
+++ MaxSubsetSumNoAdjacent.py
@@ -6,7 +6,7 @@
     Time:  O(N), where N is the number of nodes in the array
     Space (Best): O(1), when you use static variables to hold max sums
 
-    Last Practiced: 2022-03-18 06:50:30
+    Last Practiced: 2022-03-25 05:50:10
 '''
 def maxSubsetSumNoAdjacent(array):
     # If the input array is empty, 0 is the max value that can be returned
